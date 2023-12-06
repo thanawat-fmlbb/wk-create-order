f9bcf52c4c3343c330f06a4d84fcf49001340b53
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-12-07 01:40:59 +0700

    updated dockerfile and ++

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from celery import Celery
+from src.database.models import OrderInfo
+from src.database.engine import create_db_and_tables
 
 load_dotenv()
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
@@ -11,10 +13,8 @@ app = Celery("create_order_service",
              broker_connection_retry_on_startup=True)
 
 result_collector = Celery("create_order_service",
-             broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/4",
-             backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/4",
-             broker_connection_retry_on_startup=True)
+                          broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/4",
+                          backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/4",
+                          broker_connection_retry_on_startup=True)
 
-from .database.models import OrderInfo
-from .database.engine import create_db_and_tables
 create_db_and_tables()

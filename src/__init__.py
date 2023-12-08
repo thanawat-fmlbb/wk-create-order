7930f286eb20309c16646a4cf12958473ba3a59c
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-12-08 21:29:17 +0700

    tracing for create-order

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ from src.database.engine import create_db_and_tables
 
 load_dotenv()
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT = os.environ.get("REDIS_PORT", "6381")
+REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
 app = Celery("create_order_service",
              broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
              backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
--- src/tasks.py
+++ src/tasks.py
@@ -1,56 +1,62 @@
+import celery
+from opentelemetry import propagate, trace
 from sqlmodel import Session, select
 from sqlalchemy.exc import SQLAlchemyError
 from celery.exceptions import SoftTimeLimitExceeded
 
 from src import app, result_collector
-from src.database.models import OrderInfo 
+from src.database.models import OrderInfo
 from src.database.engine import get_engine
 
 RESULT_TASK_NAME = "wk-irs.tasks.send_result"
 
 
 @app.task(
-    soft_time_limit=30, 
+    soft_time_limit=30,
     time_limit=60,
     name='wk-create-order.tasks.create_order'
 )
 def create_order(**kwargs) -> bool:
-    main_id = kwargs.get('main_id', None)
-    user_id = kwargs.get('user_id', None)
-    item_id = kwargs.get('item_id', None)
-    quantity = kwargs.get('quantity', None)
+    celery.current_task.request.headers.get("traceparent")
+    tracer = trace.get_tracer(__name__)
+    ctx = propagate.extract(celery.current_task.request.headers)
+    with tracer.start_as_current_span("create_order", context=ctx):
+        main_id = kwargs.get('main_id', None)
+        user_id = kwargs.get('user_id', None)
+        item_id = kwargs.get('item_id', None)
+        quantity = kwargs.get('quantity', None)
 
-    engine = get_engine()
-    success = True
-    with Session(engine) as session:
-        try:
-            order = OrderInfo(main_id=main_id, user_id=user_id, item_id=item_id, quantity=quantity, is_valid=True)
-            session.add(order)
-            session.commit()
-        except SQLAlchemyError as e:
-            print(e)
-            order = OrderInfo(main_id=main_id, user_id=user_id, item_id=item_id, quantity=quantity, is_valid=False)
-            session.add(order)
-            session.commit()
-        except SoftTimeLimitExceeded:
-            success = False
-            kwargs["error"] = "timeout"
-        except Exception as e:
-            print(e)
-            success = False
-            kwargs["error"] = str(e)
+        engine = get_engine()
+        success = True
+        with Session(engine) as session:
+            try:
+                order = OrderInfo(main_id=main_id, user_id=user_id, item_id=item_id, quantity=quantity, is_valid=True)
+                session.add(order)
+                session.commit()
+            except SQLAlchemyError as e:
+                print(e)
+                order = OrderInfo(main_id=main_id, user_id=user_id, item_id=item_id, quantity=quantity, is_valid=False)
+                session.add(order)
+                session.commit()
+            except SoftTimeLimitExceeded:
+                success = False
+                kwargs["error"] = "timeout"
+            except Exception as e:
+                print(e)
+                success = False
+                kwargs["error"] = str(e)
 
-        result_object = {
-            "main_id": main_id,
-            "success": success,
-            "service_name": "create_order",
-            "payload": kwargs,
-        }
-        result_collector.send_task(
-            RESULT_TASK_NAME,
-            kwargs=result_object,
-            task_id=str(main_id)
-        )
+            result_object = {
+                "main_id": main_id,
+                "success": success,
+                "service_name": "create_order",
+                "payload": kwargs,
+            }
+            result_collector.send_task(
+                RESULT_TASK_NAME,
+                kwargs=result_object,
+                task_id=str(main_id)
+            )
         return success
 
 
@@ -69,10 +75,10 @@ def rollback_order(**kwargs) -> bool:
             session.commit()
     except SQLAlchemyError as e:
         kwargs["error"] = str(e)
-    
+
     result_object = {
         "main_id": main_id,
-        "success": False, # this is for triggering the rollback on the backend
+        "success": False,  # this is for triggering the rollback on the backend
         "service_name": "create_order",
         "payload": kwargs,
     }
@@ -83,8 +89,9 @@ def rollback_order(**kwargs) -> bool:
     )
     return True
 
+
 @app.task(
-    soft_time_limit=3, 
+    soft_time_limit=3,
     time_limit=60,
     bind=True,
     name='wk-create-order.tasks.test'
@@ -97,7 +104,7 @@ def test(self, **kwargs):
     except SoftTimeLimitExceeded:
         success = False
         kwargs["error"] = "timeout"
-    
+
     result_object = {
         "main_id": self.request.id,
         "success": success,
@@ -111,6 +118,7 @@ def test(self, **kwargs):
         task_id=str(self.request.id)
     )
     return result_object
+
 
 if __name__ == '__main__':
     from src.database.engine import create_db_and_tables

9436dce2ed40076d62baf5f6b7555f9e9530222f
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-12-08 21:38:42 +0700

    context added to header when sending back to irs

--- src/tasks.py
+++ src/tasks.py
@@ -1,5 +1,6 @@
 import celery
 from opentelemetry import propagate, trace
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from sqlmodel import Session, select
 from sqlalchemy.exc import SQLAlchemyError
 from celery.exceptions import SoftTimeLimitExceeded
@@ -46,6 +47,9 @@ def create_order(**kwargs) -> bool:
                 success = False
                 kwargs["error"] = str(e)
 
+            carrier = {}
+            TraceContextTextMapPropagator().inject(carrier)
+            header = {"traceparent": carrier["traceparent"]}
             result_object = {
                 "main_id": main_id,
                 "success": success,
@@ -55,77 +59,80 @@ def create_order(**kwargs) -> bool:
             result_collector.send_task(
                 RESULT_TASK_NAME,
                 kwargs=result_object,
-                task_id=str(main_id)
+                task_id=str(main_id),
+                headers=header,
             )
-        return success
+            return success
 
+    @app.task(name='wk-create-order.tasks.rollback')
+    def rollback_order(**kwargs) -> bool:
+        celery.current_task.request.headers.get("traceparent")
+        tracer = trace.get_tracer(__name__)
+        ctx = propagate.extract(celery.current_task.request.headers)
+        with tracer.start_as_current_span("rollback_order", context=ctx):
+            main_id = kwargs.get('main_id', None)
+            engine = get_engine()
+            try:
+                with Session(engine) as session:
+                    # gets corresponding payment info and user
+                    statement = select(OrderInfo).where(OrderInfo.main_id == main_id)
+                    order_info = session.exec(statement).one()
+                    order_info.is_valid = False
 
-@app.task(name='wk-create-order.tasks.rollback')
-def rollback_order(**kwargs) -> bool:
-    celery.current_task.request.headers.get("traceparent")
-    tracer = trace.get_tracer(__name__)
-    ctx = propagate.extract(celery.current_task.request.headers)
-    with tracer.start_as_current_span("rollback_order", context=ctx):
-        main_id = kwargs.get('main_id', None)
-        engine = get_engine()
+                    # commit
+                    session.commit()
+            except SQLAlchemyError as e:
+                kwargs["error"] = str(e)
+
+            carrier = {}
+            TraceContextTextMapPropagator().inject(carrier)
+            header = {"traceparent": carrier["traceparent"]}
+
+            result_object = {
+                "main_id": main_id,
+                "success": False,  # this is for triggering the rollback on the backend
+                "service_name": "create_order",
+                "payload": kwargs,
+            }
+            result_collector.send_task(
+                RESULT_TASK_NAME,
+                kwargs=result_object,
+                task_id=str(main_id),
+                headers=header,
+            )
+        return True
+
+    @app.task(
+        soft_time_limit=3,
+        time_limit=60,
+        bind=True,
+        name='wk-create-order.tasks.test'
+    )
+    def test(self, **kwargs):
+        from time import sleep
         try:
-            with Session(engine) as session:
-                # gets corresponding payment info and user
-                statement = select(OrderInfo).where(OrderInfo.main_id == main_id)
-                order_info = session.exec(statement).one()
-                order_info.is_valid = False
-
-                # commit
-                session.commit()
-        except SQLAlchemyError as e:
-            kwargs["error"] = str(e)
+            success = True
+            sleep(10)
+        except SoftTimeLimitExceeded:
+            success = False
+            kwargs["error"] = "timeout"
 
         result_object = {
-            "main_id": main_id,
-            "success": False,  # this is for triggering the rollback on the backend
-            "service_name": "create_order",
+            "main_id": self.request.id,
+            "success": success,
+            "service_name": "inventory",
             "payload": kwargs,
         }
+
         result_collector.send_task(
             RESULT_TASK_NAME,
             kwargs=result_object,
-            task_id=str(main_id)
+            task_id=str(self.request.id)
         )
-    return True
+        return result_object
 
+    if __name__ == '__main__':
+        from src.database.engine import create_db_and_tables
 
-@app.task(
-    soft_time_limit=3,
-    time_limit=60,
-    bind=True,
-    name='wk-create-order.tasks.test'
-)
-def test(self, **kwargs):
-    from time import sleep
-    try:
-        success = True
-        sleep(10)
-    except SoftTimeLimitExceeded:
-        success = False
-        kwargs["error"] = "timeout"
-
-    result_object = {
-        "main_id": self.request.id,
-        "success": success,
-        "service_name": "inventory",
-        "payload": kwargs,
-    }
-
-    result_collector.send_task(
-        RESULT_TASK_NAME,
-        kwargs=result_object,
-        task_id=str(self.request.id)
-    )
-    return result_object
-
-
-if __name__ == '__main__':
-    from src.database.engine import create_db_and_tables
-
-    # inputs are so that we can have pauses
-    create_db_and_tables()
+        # inputs are so that we can have pauses
+        create_db_and_tables()
